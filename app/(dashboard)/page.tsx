807640e034c2c9e5d06f77b31f1d7d6a61eb4245
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2024-03-17 15:45:05 +0600

    adding board schema

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import { UserButton } from "@clerk/nextjs";
+import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
+import BoardList from "./_components/board-list";
 
-export default function Home() {
-  const tasks = useQuery(api.tasks.get);
+interface DashboardProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export default function Dashboard({ searchParams }: DashboardProps) {
+  const { organization } = useOrganization();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <EmptyOrg />
+    <main className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization && <EmptyOrg />}
+      {organization && (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </main>
   );
 }
